63a4f06cf793fe86301231d0540337dbc5e93299
johnyu763 <johny11@uci.edu>
2019-10-12 14:54:55 -0700

    first commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by John Yu on 10/12/19.
+//  Copyright © 2019 John Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
