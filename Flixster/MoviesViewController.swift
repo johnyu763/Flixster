9c7fd5b2c53a67cefb76df296d4eab09dc019a60
johnyu763 <johny11@uci.edu>
2019-10-25 18:34:00 -0700

    second screen

--- Flixster/MoviesViewController.swift
+++ Flixster/MoviesViewController.swift
@@ -66,5 +66,20 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         return cell
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        print("Loading up details scene")
+    
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
